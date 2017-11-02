16751db7bc0d17ec7feef739574f0d3ef5a2fe83
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2017-11-03 00:03:31 +0800

    no message

--- server.js
+++ server.js
@@ -9,67 +9,20 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.engine('html', swig.renderFile);
 app.set('view engine', 'html');
 app.set('views', __dirname + '/');
+
 app.use('/dist', express.static('dist'));
 app.use('/lexiugo', express.static('dist/lexiugo'));
 app.use('/server/dist', express.static('dist'));
 app.use('/server/lexiugo', express.static('dist/server/lexiugo'));
 
-console.log(process.env.NODE_ENV,'nodeserver=jj ');
 if(process.env.NODE_ENV ==='dev'){
-
+    global.ripath='/dist/';
 }else{
-    var ripath='/server/dist/';
+    global.ripath='/server/dist/';
 }
 
-app.use('/lexiugo-app', require('./api'));
-app.get('/server/:id',(req,res,next)=>{
-    if(req.params.id == 'isAdd'){
-        var data=req.query;
-        var url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='+data.token+'&openid='+data.openid+'&lang=zh_CN'
-        console.log(data,url);
-        superagent
-            .get(url)
-            .accept('json')
-            .set('X-API-Key', 'foobar')
-            .set('Accept', 'application/json')
-            .end(function(reqe,rese){
-                console.log(rese.body);
-                res.json(rese.body)
-            });
-    }else {
-        console.log(req.query);
-        var arr = {
-            loveCarRepair: '维修记录',
-            lexiuApp: '修理厂',
-            reportStatistics: '透明修车',
-            newBuild: '案件推修',
-            repairState: '运营管理',
-            integral:'积分榜'
-        }
-        var dataList = {//'http://qq328532063.6655.la/dist/'+req.query.action ||
-            path: 'http://www.toumingxiuche.cn/server/dist/' + req.query.action || ripath + (req.query.action || 'lexiuApp'),
-            title: arr[req.query.action]
-        }
-        res.render('index', {dataList: dataList});
-    }
-})
-app.get('/server',(req,res,next)=>{
-    console.log(req.query);
-    var arr={
-        loveCarRepair:'维修记录',
-        lexiuApp:'修理厂',
-        reportStatistics:'透明修车',
-        newBuild:'案件推修',
-        repairState:'运营管理',
-        integral:'积分榜'
-    }
-    var dataList={//'http://qq328532063.6655.la/dist/'+req.query.action ||
-        path:'http://www.toumingxiuche.cn/server/dist/'+req.query.action || ripath+(req.query.action || 'lexiuApp'),
-        title:arr[req.query.action]
-    }
-    res.render('index',{dataList:dataList});
-})
-//app.use('/lexiugo-app',require('./api'))  ;
+app.use('/lexiugo-app', require('./api'));//toumingxiu/
+app.use('/server', require('./serverApi'));
 
 
 
---
+++ serverApi/index.js
@@ -0,0 +1,43 @@
+var express= require('express');
+var superagent = require('superagent');
+
+var router = express.Router();
+//express body-parser swig iconv-lite bluebird request
+var projjj=true;
+
+//首页
+router.get('/showEWM/:id',(req,res,next)=>{
+    if(req.params.id == 'isAdd'){
+        var data=req.query;
+        var url='https://api.weixin.qq.com/cgi-bin/user/info?access_token='+data.token+'&openid='+data.openid+'&lang=zh_CN'
+        console.log(data,url);
+        superagent
+            .get(url)
+            .accept('json')
+            .set('X-API-Key', 'foobar')
+            .set('Accept', 'application/json')
+            .end(function(reqe,rese){
+                console.log(rese.body);
+                res.json(rese.body)
+            });
+    }else{
+        res.cookie('jb','ii')
+        var arr={loveCarRepair:'维修记录',lexiuApp:'修理厂',reportStatistics:'透明修车',newBuild:'案件维修',repairState:'运营管理',integral:'积分榜'}
+        var dataList={
+            path:ripath+(req.query.action || 'lexiuApp'),
+            title:arr[req.query.action]
+        }
+        res.render('index',{dataList:dataList});
+    }
+});
+router.get('/',(req,res,next)=>{
+    console.log(req.query);
+    res.cookie('jb','ii')
+    var arr={loveCarRepair:'维修记录',lexiuApp:'修理厂',reportStatistics:'透明修车',newBuild:'案件维修',integral:'积分榜'}
+    var dataList={
+        path:ripath+(req.query.action || 'lexiuApp'),
+        title:arr[req.query.action]
+    }
+    res.render('index',{dataList:dataList});
+});
+module.exports = router;
