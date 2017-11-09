46e54d6c6b858d09f23ee9d754879abfaa467ab7
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2017-11-10 01:37:12 +0800

    no message

--- serverApi/index.js
+++ serverApi/index.js
@@ -1,9 +1,14 @@
 var express= require('express');
 var superagent = require('superagent');
-
+var jsonp = require('superagent-jsonp')
+var sql=require('./config/ConnectDatabase');
+var http=require('http')
+var $= require('jquery')
 var router = express.Router();
 //express body-parser swig iconv-lite bluebird request
 var projjj=true;
+
+
 
 //首页
 router.get('/showEWM/:id',(req,res,next)=>{
@@ -31,7 +36,6 @@ router.get('/showEWM/:id',(req,res,next)=>{
     }
 });
 router.get('/',(req,res,next)=>{
-    console.log(req.query);
     res.cookie('jb','ii')
     var arr={loveCarRepair:'维修记录',lexiuApp:'修理厂',reportStatistics:'透明修车',newBuild:'案件维修',integral:'积分榜'}
     var dataList={
@@ -40,4 +44,54 @@ router.get('/',(req,res,next)=>{
     }
     res.render('index',{dataList:dataList});
 });
+router.post('/selectCKImg',(req,res,next)=>{
+    var query = (connection)=>{
+        sql.query({
+            connection:connection,
+            sql:"SELECT ID FROM lx_workmainsheet WHERE REPORTNO ='"+req.body.reportno+"' AND PLATENO = '"+req.body.plateno+"' AND DELFLAG='0' AND PUSHTASKNO='"+req.body.pushtaskno+"'",
+            fun:(dat)=>{
+                console.log(dat);
+                sql.query({
+                    connection:connection,
+                    sql:"SELECT * from lx_sheetpicture WHERE WORKID='"+dat+"'",
+                    success:(dats)=>{
+                        var m=[];
+                        for(var i in dats){
+                            m.push({
+                                id:dats[i].ID,
+                                ImgPath:'/damagePicture/'+dats[i].ZZBH+'/'+dats[i].WORKNO+'/chakan/small/'+dats[i].DAMAGEPICTURE,
+                                type:dats[i].DAMAGEAREAS
+                            })
+                        }
+                        res.jsonp(m)
+                    }
+                })
+            }
+        })
+    }
+    sql.Connect(query)
+})
+
+
+router.post('/BQXX',(req,res,next)=>{
+    console.log(req.body.data);
+    var url="http://assess-api.lexiugo.com/assess-api/assess-api"+encodeURIComponent(req.body.data)+"?callback=__callback&userName=lexiugo&passwd=n27H3lNGL7wJSePFsrr0g16UTU0%2BtDfsGHMVZ2pmxsDaFV4cVSzVwQ%3D%3D&_=1510119995854"
+    http.get(url,(r)=>{
+        var html = '';
+        // 绑定data事件 回调函数 累加html片段
+        r.on('data',(data)=>{
+            html += data;
+        });
+        r.on('end',()=>{
+            console.log(eval(''+html.split('__callback')[1]+''))
+            res.json(eval(''+html.split('__callback')[1]+''))
+        });
+    }).on('error',()=>{
+        console.log('获取数据错误');
+    });
+})
+
+
+
+
 module.exports = router;
