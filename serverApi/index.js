04ad3b960309658d87b13d844b5b0d9b49d0f640
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2018-02-05 19:27:34 +0800

    no message

--- serverApi/index.js
+++ serverApi/index.js
@@ -407,7 +407,7 @@ router.post('/pcMapXlc',(req,res,next)=>{
     "<input type='hidden' id='brandCodeNode' value='"+req.body.brandCode+"'>" +
     "<input type='hidden' id='MapUseUserId' value='"+req.body.userId+"'>" +
     "<input type='hidden' id='cityName' value='"+req.body.cityName+"'>" +
-    "<script src='http://116.62.162.134:8090/server/dist/pcSelectMap.js'></script>" +
+    "<script src='/server/dist/pcSelectMap.js'></script>" +
     "</div>"
     })
 })
