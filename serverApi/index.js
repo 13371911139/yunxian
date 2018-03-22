953de3e5f08c61c95991eaac2d5969ac5a772ff7
LAPTOP-SKT2CH5S\feiqu <328532063@qq.com>
2018-03-22 11:00:10 +0800

    no message

--- serverApi/index.js
+++ serverApi/index.js
@@ -287,7 +287,7 @@ router.post('/selectWXImg',(req,res,next)=>{
 router.get('/down',(req,res,next)=>{
     var ua = req.headers['user-agent'],vUrl
     if (/Android/.test(ua)){
-        vUrl='https://www.pgyer.com/QA1D'
+        vUrl='https://www.pgyer.com/PKVF'
     }else{
         vUrl='https://itunes.apple.com/us/app/%E9%80%8F%E6%98%8E%E4%BF%AE%E8%BD%A6/id1271183608?l=zh&ls=1&mt=8'
     }
